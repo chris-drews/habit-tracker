3d76f884975c5c81e3c5d8a1de8b0b28fa7e69f3
chris-drews <74363901+chris-drews@users.noreply.github.com>
2023-06-13 15:51:18 +0200

    Improved Tests
    Improved tests; added a section to make sure that the database exists

--- test_habit_app.py
+++ test_habit_app.py
@@ -1,9 +1,13 @@
+import database_setup
+import os
+
+#create database if necessary
+if not "habit_data.db" in os.listdir():
+    database_setup.main("habit_data.db")
+
+
+from unittest import mock, TestCase, main
 import tracker as tf
-from unittest import mock, TestCase, main, TestLoader
-
-
-#disable sorting
-TestLoader.sortTestMethodsUsing = None
 
 
 class TestHabits(TestCase):
@@ -11,53 +15,59 @@ class TestHabits(TestCase):
         habits = tf.load_habits()
         assert type(habits) == list
 
-    #creation
-    def test_habit_creation(self):
+    #creation, deletion, streak function
+    def test_new_habit(self):
+        #creation
         with mock.patch("builtins.input", return_value="d"):
             new_habit = tf.create_habit("test_habit")
         assert new_habit.name == "test_habit"
 
-        #save test habit in preparation for next tests
         habits = tf.load_habits()
         habits.append(new_habit)
+
+        #does saving habits work?
         tf.commit_changes(habits)
         habits = tf.load_habits()
-        for habit in habits:
-            print(habit.name)
+        test_habit = tf.get_habit("test_habit", habits)
+        assert test_habit != None
 
-    #streak functionality        
-    def test_streak_complete(self):
-        habits = tf.load_habits()
-        tf.get_habit("test_habit", habits).complete()
+        #streak functionality
+        assert test_habit.streak == 0
+        test_habit.complete()
+        assert test_habit.streak == 1
+
+        #deletion
+        assert tf.get_habit("test_habit", habits) != None
+        with mock.patch("builtins.input", return_value="y"): #there is a user input here that needs to be simulated
+            tf.delete_habit(name = "test_habit", habits = habits)
+        tf.commit_changes(habits)
+
+        #reload again to check
+        habits = tf.load_habits()#
+        names = [habit.name for habit in habits]
+        assert "test_habit" not in names
 
     #analytic tests
     def test_habit_overview(self):
         habits = tf.load_habits()
         tf.view_all(frequency="daily", habits=habits)
 
-    def test_longest_streak(self):
+    def test_longest_current_streak(self):
         habits = tf.load_habits()
         result = tf.longest_current_streak(habits)
-        assert result != None
+        assert result == [['Take a shower', 10], ['Go to the gym', 3]]
 
     def test_longest_all_time_streak(self):
         habits = tf.load_habits()
         result = tf.longest_all_time_streak(habits)
-        assert result != None
+        assert result == ("Take a shower", 15)
 
     def test_hardest_streak(self):
         habits = tf.load_habits()
         result = tf.hardest_streak(habits)
-        assert result != None
+        assert result == ('Go for a walk', 6)
 
-    #deletion
-    def test_habit_deletion(self):
-        habits = tf.load_habits()
-        assert tf.get_habit("test_habit", habits) != None
-        with mock.patch("builtins.input", return_value="y"):
-            tf.delete_habit(name = "test_habit", habits = habits)
-        tf.commit_changes(habits)
-
+    
 
 if __name__ == "__main__":
     main()
